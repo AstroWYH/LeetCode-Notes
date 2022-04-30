570c3a17a01ceb93f1cfcc7241a38f4746dcb909
wangyuhang <819369336@qq.com>
2022-04-30 15:03:23 +0800

    [fix] 滑动窗口 快慢双指针 2022-0430

--- 209.长度最小的子数组.cpp
+++ 209.长度最小的子数组.cpp
@@ -1,3 +1,14 @@
+滑动窗口 快慢双指针
+
+思路：经典滑动窗口，快慢双指针题型。for遍历fast，sum累加；当满足一定条件时，slow++，缩减窗口，
+同时更新sum和ans。运用标准的滑动窗口模板就行。
+
+注：slow移动的判断条件，如while (sum >= target)，往往不会太过复杂，如果考虑得太过复杂的话，比如
+最下面硬想的方法，往往已经陷入误区，应该进一步考虑找到简单有效的判定条件。
+
+1：注意这里的while，slow++会一直进行到不满足条件为止；这里有时是while有时是if，具体情况具体分析。
+2：特殊情况，全部都不满足条件，返回0。
+
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
@@ -5,14 +16,14 @@ public:
         int sum = 0, ans = INT32_MAX, diff = 0;
         for (; fast < nums.size(); fast++) {
             sum += nums[fast];
-            while (sum >= target) {
+            while (sum >= target) { // wyh 1
                 diff = fast - slow + 1;
                 ans = min(diff, ans);
                 sum -= nums[slow];
                 slow++;
             }
         }
-        if (slow == 0) return 0;
+        if (slow == 0) return 0; // wyh 2
         return ans;
     }
 };
