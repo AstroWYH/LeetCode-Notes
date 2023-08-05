bd524f984ab5a0a2e4b67bc01760e85ef16504c0
AstroWYH <94472801+AstroWYH@users.noreply.github.com>
2023-08-05 12:08:32 +0800

    [复习] 二叉树 前序遍历 递归 DFS 深度优先遍历 2023-0805

--- 236.二叉树的最近公共祖先.cpp
+++ 236.二叉树的最近公共祖先.cpp
@@ -53,3 +53,33 @@ public:
         return root; // 2 if(left != null and right != null)
     }
 };
+
+// 复习
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* node, TreeNode* p, TreeNode* q) {
+      if (node == nullptr) return nullptr;
+      if (node == p || node == q) return node;
+
+      TreeNode* left = dfs(node->left, p, q);
+      TreeNode* right = dfs(node->right, p, q);
+
+      if (left == nullptr && right == nullptr) return nullptr;
+      if (right == nullptr) return left;
+      if (left == nullptr) return right;
+      return node;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return dfs(root, p, q);
+    }
+};
