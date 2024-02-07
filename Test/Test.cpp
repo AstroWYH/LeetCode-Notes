efb6479a61e4204d45f2912400f96689c828ba6f
astrowang <819369336@qq.com>
2024-02-07 18:21:06 +0800

    Init Config

---
+++ Test/Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+    string c = "hello cpp";
+    cout << "hello c++" << endl;
+    return 0;
+}
