fcf4776410510b66b3930a6348c30ffac2447b07
819369336@qq.com <819369336@qq.com>
2025-02-07 14:52:43 +0800

    up

--- 216.combination-sum-iii.cpp
+++ 216.combination-sum-iii.cpp
@@ -88,37 +88,37 @@ using namespace std;
 //   }
 // };
 
-// 写法2是错误的，这个表达式的判断存在问题
+// 写法2是错误的，这个表达式的判断存在问题，问题出在path的size那里，给path加一下int就对了
 
-class Solution {
-public:
-  vector<vector<int>> combinationSum3(int k, int n) {
-    vector<int> path;
-    vector<vector<int>> ans;
-    dfs(k, n, 1, path, ans);
-    return ans;
-  }
+// class Solution {
+// public:
+//   vector<vector<int>> combinationSum3(int k, int n) {
+//     vector<int> path;
+//     vector<vector<int>> ans;
+//     dfs(k, n, 1, path, ans);
+//     return ans;
+//   }
 
-private:
-  void dfs(int k, int n, int start, vector<int> &path,
-           vector<vector<int>> &ans) {
-    if (path.size() == k && n == 0) {
-      ans.push_back(path);
-      return;
-    }
+// private:
+//   void dfs(int k, int n, int start, vector<int> &path,
+//            vector<vector<int>> &ans) {
+//     if (path.size() == k && n == 0) {
+//       ans.push_back(path);
+//       return;
+//     }
 
-    if (n < 0)
-      return;
+//     if (n < 0)
+//       return;
     
-    // 写法1：j <= 9 - (k - path.size()) + 1
-    // 写法2：(9 - j + 1) >= (k - path.size()) 错误
-    for (int j = start; j <= 9 - (k - path.size()) + 1; j++) {
-      path.push_back(j);
-      dfs(k, n - j, j + 1, path, ans);
-      path.pop_back();
-    }
-  }
-};
+//     // 写法1：j <= 9 - (k - path.size()) + 1
+//     // 写法2：(9 - j + 1) >= (k - path.size()) 错误 (9 - j + 1) >= (k - (int)path.size())
+//     for (int j = start; j <= 9 - (k - path.size()) + 1; j++) {
+//       path.push_back(j);
+//       dfs(k, n - j, j + 1, path, ans);
+//       path.pop_back();
+//     }
+//   }
+// };
 
 // class Solution {
 // private:
@@ -149,6 +149,34 @@ private:
 //         return result;
 //     }
 // };
+
+// 灵神解法，倒序更容易理解剪枝
+// 灵神的剪枝很彻底，还有一个t > (i * 2 - d + 1) * d / 2，表达的意思是
+// 剩余的数全部加起来也不够大，就剪掉，这是用了规律得出的公式
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        auto dfs = [&](this auto&& dfs, int i, int t) {
+            int d = k - path.size(); // 还要选 d 个数
+            if (t < 0 || t > (i * 2 - d + 1) * d / 2) { // 剪枝
+                return;
+            }
+            if (d == 0) { // 找到一个合法组合
+                ans.emplace_back(path);
+                return;
+            }
+            for (int j = i; j >= d; j--) {
+                path.push_back(j);
+                dfs(j - 1, t - j);
+                path.pop_back();
+            }
+        };
+        dfs(9, n);
+        return ans;
+    }
+};
 // @lc code=end
 
 /*
