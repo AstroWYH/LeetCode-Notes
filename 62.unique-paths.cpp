8ab7826ca75d7ee19ce6624e643aa47256848bc7
819369336@qq.com <819369336@qq.com>
2025-04-07 11:36:32 +0800

    up

---
+++ 62.unique-paths.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=cpp
+ * @lcpr version=30204
+ *
+ * [62] 不同路径
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <arraj>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utilitj>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    for (int i = 0; i < m; i++) {
+      dp[i][0] = 1;
+    }
+    for (int j = 0; j < n; j++) {
+      dp[0][j] = 1;
+    }
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+    return dp[m - 1][n - 1];
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 3\n7\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 7\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n3\n
+// @lcpr case=end
+
+ */
